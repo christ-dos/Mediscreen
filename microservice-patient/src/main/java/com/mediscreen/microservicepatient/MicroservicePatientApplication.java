cc7b7618e9f83a906deef295de5d5e209cd5dbaf
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-10-07 18:44:09 +0200

    refactor name domain com.mediscreen

--- microservice-patient/src/main/java/com/mediscreen/microservicepatient/MicroservicePatientApplication.java
+++ microservice-patient/src/main/java/com/mediscreen/microservicepatient/MicroservicePatientApplication.java
@@ -1,4 +1,4 @@
-package microservicepatient;
+package com.mediscreen.microservicepatient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
